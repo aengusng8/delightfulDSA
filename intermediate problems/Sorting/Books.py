58f6ef927ac6b97bc80626d8ec2ecd18d5a48c25
Nguyễn Đức Anh <67547213+xxnguyenducanh@users.noreply.github.com>
2021-06-13 16:11:15 +0700

    Update Books.py

--- intermediate problems/Sorting/Books.py
+++ intermediate problems/Sorting/Books.py
@@ -1,12 +1,15 @@
 N, T = map(int, input().split())
 A = list(map(int, input().split()))
+
 max_book = 0
 read_time = 0
 start_book = 0
+
 for end_book in range(N):
 	read_time += A[end_book]
 	while read_time > T:
 		read_time -= A[start_book]
 		start_book += 1
 	max_book = max(max_book, end_book - start_book + 1)
+	
 print(max_book)
